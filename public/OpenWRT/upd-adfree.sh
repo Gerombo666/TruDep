4b8b63205a0c17e005619e16da6f771d6529481a
Roman Martynenko <43039611+Roman-Martynenko@users.noreply.github.com>
2020-03-25 22:35:20 +0300

    Delete upd-adfree.sh

--- public/OpenWRT/upd-adfree.sh
+++
@@ -1,7 +0,0 @@
-##adfree
-wget http://winhelp2002.mvps.org/hosts.txt -O /tmp/adfree-tmp
-sed 's/^\(.*\).$/\1/' /tmp/adfree-tmp > /tmp/adfree
-rm -f /tmp/adfree-tmp
-
-## dns restart to update
-/etc/init.d/dnsmasq restart
